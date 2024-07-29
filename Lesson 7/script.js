07a5f52e968d8709f5accc483c3ec2b72f196d05
Valerii Hrebennikov <valerii.hrebennikov@hebron-academy.com>
2024-07-30 00:19:46 +0300

    Tasks

--- Lesson 7/script.js
+++ Lesson 7/script.js
@@ -11,3 +11,37 @@ console.log(colors[2]);
 // Зберіг у змінній щоб у подальшому якщо б задобився цей елемент щоб його використати
 let popColors = colors.pop();
 console.log(colors);
+
+// Task 3
+const fruits = ["яблуко", "банан", "апельсин", "груша", "ківі"];
+// Це не з Gpt chat, про цей метод я дізнався на одному курсі
+const sort = fruits.sort((a, b) => {
+    return a[0].localeCompare(b[0])
+});
+
+console.log(fruits);
+
+// Join
+const joinString = fruits.join(", ");
+console.log(joinString);
+
+// Filter
+const filter = fruits.filter((value) => {
+    return value.length > 5
+});
+
+console.log(filter);
+
+// Map
+const map = fruits.map((value) => {
+    return value.toUpperCase()
+});
+
+console.log(map);
+
+// Reduce
+const reduce = fruits.reduce((acc, value) => {
+    return acc + "-" + value
+});
+
+console.log(reduce);
