67033d357b7044d195e93eb2d3fa6edc2c0e2999
Valerii Hrebennikov <valerii.hrebennikov@hebron-academy.com>
2024-07-29 23:29:57 +0300

    Tasks

--- Lesson 7/script.js
+++ Lesson 7/script.js
@@ -3,3 +3,11 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 numbers.push(11, 12, 13, 14, 15);
 
 console.log(numbers);
+
+// Task 2
+const colors = ["червоний", "синій", "зелений", "жовтий", "чорний"];
+console.log(colors[2]);
+
+// Зберіг у змінній щоб у подальшому якщо б задобився цей елемент щоб його використати
+let popColors = colors.pop();
+console.log(colors);
