418c660b28b81b2bccc676cff1624aa89e58b95e
Valerii Hrebennikov <valerii.hrebennikov@hebron-academy.com>
2024-08-05 16:28:36 +0300

    Add tasks

--- Lesson 9/script.js
+++ Lesson 9/script.js
@@ -6,16 +6,38 @@ function calculateArea (width, height) {
 console.log(calculateArea(10, 5)); 
 
 // Task 2
-function printMessage (message) {
+function printMessage(message) {
     console.log(message);
 }
 
-printMessage("Hello World!")
-
-function sendMessage (message, callback) {
-   callback(message)
+function sendMessage(message, callback) {
+    // Якщо просто виклакати callback пише is not function at sendMessage
+    // знайшов така рішення, не знаю чи це правильно, але працює
+    if (typeof callback === "function") {
+        callback(message);
+    } 
 }
 
-sendMessage("I'm Valerii");
-sendMessage("I am Front-end developer");
-sendMessage(printMessage());
+sendMessage("Hello, world!", printMessage);
+sendMessage("I'm Valerii", printMessage);
+sendMessage("I am a Front-end developer", printMessage);
+
+
+//  Task 3
+const Myfunction = function (num) {
+    console.log(num * num);
+  };
+
+  Myfunction(4);
+  Myfunction(10);
+  Myfunction(13);
+
+  function checkOnNumber(number) {
+    if (typeof number === "number") {
+        console.log(`${number} - це число`);
+    } else {
+        console.log(`${number} - це не число`);
+    }
+  }
+
+checkOnNumber(5)
